03915fbe81d57a192755aa920954a74db59364b9
sahilkashyap <sahilkashyap64@gmail.com>
2020-02-22 18:59:48 +0530

    Flash Card component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlashComponent } from './flash/flash.component';
+import { FlashService } from './flash.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, FlashComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [FlashService]
 })
 export class AppModule { }
